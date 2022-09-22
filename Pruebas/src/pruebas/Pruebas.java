934bf6339ac0862bb89e952b209e7a5e6bc510bd
rubenfayos <fayosruben@gmail.com>
2022-09-22 10:25:56 +0200

    Hacer en casa implementando calendar

---
+++ Pruebas/src/pruebas/Pruebas.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM 2
+ */
+public class Pruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Introduce tu fecha de nacimiento: ");
+        
+        Calendar c = new Calendar();
+       
+
+    }
+    
+}
